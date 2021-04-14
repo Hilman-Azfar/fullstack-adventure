8d6764e45d4afd2db030dae63feaa7feba71dcb4
Hilman Azfar <m.hilman.azfar@gmail.com>
2021-04-14 23:37:33 +0800

    [HA] add data for categories and popular

---
+++ src/data.js
@@ -0,0 +1,37 @@
+const Categories = ["$0 delivery", "near me", "same prices in-store", "whole cakes", "islandwide delivery", "michelin-recommended"];
+const Popular = [
+    {
+        title: "macdonalds",
+        tags: ["chicken", "fast food", "halal", "burger"],
+        rating: 4.2,
+        deliveryTime: 21,
+        distance: 1600
+    },
+    {
+        title: "nana original thai",
+        tags: ["Thai",  "Islandwide Delivery",  "Seafood",  "Asian"],
+        rating: 4.1,
+        deliveryTime: 33,
+        distance: 2800
+    },
+    {
+        title: "Live Seafood - Porridge - Syed Alwi Road [Islandwide Delivery]",
+        tags: ["Chinese",  "BBQ",  "Seafood",  "Porridge",  "Snack"],
+        rating: 4.3,
+        deliveryTime: 31,
+        distance: 3400
+    },
+    {
+        title: "Tiong Shian Porridge Centre - New Bridge Road",
+        tags: ["Local",  "Asian",  "Chinese"],
+        rating: 4.4,
+        deliveryTime: 28,
+        distance: 2300
+    }
+]
+
+export const stores = {
+    Categories,
+    Popular
+}
+
