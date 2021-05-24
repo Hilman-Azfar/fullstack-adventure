438fb2c3f62c52a80071647a2c923d0fd3bee7ad
Toruman <ngtruman33@gmail.com>
2021-05-24 21:30:57 +0800

    [oh no]

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <Nav></Nav>
+      <Carousel></Carousel>
+      <Carousel></Carousel>
+    </>
+  )
+}
+
+function Nav () {
+
+}
+
+function Carousel () {
+
+}
+
+export default App;
